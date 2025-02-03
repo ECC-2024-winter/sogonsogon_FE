2ffb6a69c7d9e997d5d60b05c97f1dc444d1fbe8
Ayun Jeong <ayun01j07@gmail.com>
2025-02-03 23:03:18 +0900

    chore: 폴더 구조 변경

---
+++ frontend/src/components/Calendar.jsx
@@ -0,0 +1,29 @@
+import React, { forwardRef, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import '../styles/Calendar.css'
+import {ko} from "date-fns/locale";
+
+const Calendar = () => {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const ExampleCustomInput = forwardRef(
+        ({ value, onClick, className }, ref) => (
+          <button className={className} onClick={onClick} ref={ref}>
+            {value}
+          </button>
+        ),
+      );
+
+        return (
+                <DatePicker
+                    locale={ko}
+                    showPopperArrow={false}
+                    selected={selectedDate}
+                    onChange={(date)=>setSelectedDate(date)}
+                    customInput={<ExampleCustomInput className="example-custom-input"/>}
+                    dateFormat="yyyy.MM.dd"
+                />
+        );
+};
+
+export default Calendar;
