a6af99bca27c1b64f75efce066ee5aad2aa5d540
Ayun Jeong <ayun01j07@gmail.com>
2025-02-04 21:30:58 +0900

    chore: 로그인 페이지 라우팅 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
       <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </Router>
