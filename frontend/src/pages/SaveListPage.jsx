888600b5ffab41b90e64fd65efe4fc6d5a174bcc
hyunsuh <sunnie0704@gmail.com>
2025-02-04 21:34:57 +0900

    feat: 저장 목록 페이지 구현

---
+++ frontend/src/pages/SaveListPage.jsx
@@ -0,0 +1,68 @@
+import Header from "../components/Header";
+import SaveFolder from "../components/SaveFolder";
+import "../styles/SaveListPage.css";
+
+/*가상 데이터*/
+const folders = [
+    {
+        id: 1,
+        folderName: "혼밥 맛집",
+    },
+    {
+        id: 2,
+        imageUrl1: "https://blog.kakaocdn.net/dn/dizeYM/btrN5vZONwk/0ShfJor6t6KANhKzI3Qr1k/img.jpg",
+        folderName: "북카페/ 작업하기 좋은 카페",
+    },
+    {
+        id: 3,
+        folderName: "2025년 1월 전시회",
+    },
+    {
+        id: 4,
+        folderName: "망원동 소품샵",
+    },
+    {
+        id: 5,
+    },
+    {
+        id: 6,
+    },
+    {
+        id: 7,
+    },
+    {
+        id: 8,
+    },
+    {
+        id: 9,
+    },
+    {
+        id: 10,
+    },
+];
+
+
+function SaveListPage() {
+    return (
+        <div className="save-list-page">
+           <Header />
+            <div className="save-title">저장 목록</div>
+                <div className="save-folders">
+                    <div className="save-folder-container">
+                        {folders.map((folder) => (
+                            <SaveFolder
+                                key={folder.id}
+                                imageUrl1={folder.imageUrl1}
+                                imageUrl2={folder.imageUrl2}
+                                imageUrl3={folder.imageUrl3}
+                                imageUrl4={folder.imageUrl4}
+                                folderName={folder.folderName}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+    );
+};
+
+export default SaveListPage;
