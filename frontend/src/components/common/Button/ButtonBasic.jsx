36ec09b9df5b35f412367f78ae90198d90a44e0d
hyunsuh <sunnie0704@gmail.com>
2025-02-11 00:14:05 +0900

    feat: 삭제 및 추가 아이콘 구현

---
+++ frontend/src/components/common/Button/ButtonBasic.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+function Button({ disabled, children }) {
+  return <StyledButton disabled={disabled}>{children}</StyledButton>;
+}
+
+const StyledButton = styled.button`
+  display: inline-flex;
+  align-items: center;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  color: #000000;
+  font-weight: 500;
+  font-size: 16px;
+  font-family: 'Pretendard';
+  cursor: pointer;
+  padding: 2px;
+  background: #ffffff;
+
+  &:active,
+  &:hover {
+    background: var(--button-hover-bg-color, rgb(247, 244, 244));
+  }
+`;
+
+export default Button;
