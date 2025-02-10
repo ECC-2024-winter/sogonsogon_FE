5c705ecea8da14861095f39ba4e0395f4111b5ab
hyunsuh <sunnie0704@gmail.com>
2025-02-10 16:14:16 +0900

    chore: import React 삭제

--- frontend/src/components/common/Rating.jsx
+++ frontend/src/components/common/Rating.jsx
@@ -1,23 +1,30 @@
-import React from "react";
 import { useState } from "react";
 import { PiStarFill, PiStarLight } from "react-icons/pi";
 
 function Rating() {
-    const [ rating, setRating ] = useState(0);
+  const [rating, setRating] = useState(0);
 
-    return (
-        <div>
-            {[...Array(rating)].map((a, i) =>(
-                <PiStarFill className="rating-star" color="#FFBF87" key={i} onClick={() => setRating( i + 1 )} />
-            ))}
+  return (
+    <div>
+      {[...Array(rating)].map((a, i) => (
+        <PiStarFill
+          className="rating-star"
+          color="#FFBF87"
+          key={i}
+          onClick={() => setRating(i + 1)}
+        />
+      ))}
 
-            {[...Array(5-rating)].map((a, i) =>(
-                <PiStarLight className="rating-star" color="#FFBF87" key={i} onClick={() => setRating( rating + i + 1 )} />
-            ))}
-        </div>
-    );
-
-    
-};
+      {[...Array(5 - rating)].map((a, i) => (
+        <PiStarLight
+          className="rating-star"
+          color="#FFBF87"
+          key={i}
+          onClick={() => setRating(rating + i + 1)}
+        />
+      ))}
+    </div>
+  );
+}
 
 export default Rating;
