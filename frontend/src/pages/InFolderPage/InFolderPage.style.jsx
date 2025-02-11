bbe2f8da56ee8d960f69fa45e6da8c41a602164f
hyunsuh <sunnie0704@gmail.com>
2025-02-11 18:45:53 +0900

    feat: 폴더를 눌렀을 때 뜨는 목록 페이지 구현

---
+++ frontend/src/pages/InFolderPage/InFolderPage.style.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const PageTop = styled.div`
+  display: flex;
+  margin-top: 50px;
+  margin-left: 200px;
+`;
+
+export const BackIcon = styled.div`
+  font-size: 32px;
+  margin-right: 20px;
+`;
+export const SaveTitle = styled.div`
+  color: #000000;
+  font-size: 32px;
+  font-family: 'Pretendard';
+  font-weight: 600;
+  text-align: left;
+`;
+
+export const PlaceCardContainer = styled.div`
+  display: grid;
+  grid-template-columns: 261.33px 261.33px 261.33px 261.33px 261.33px;
+  gap: 24px;
+  border: none;
+  margin-left: 250px;
+  margin-right: 250px;
+  margin-top: 50px;
+`;
