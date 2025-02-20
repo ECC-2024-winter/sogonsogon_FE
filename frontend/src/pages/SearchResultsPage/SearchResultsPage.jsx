8322d7c463ebdfe9cf38adf9738b553f1ed35158
Ayun Jeong <ayun01j07@gmail.com>
2025-02-20 21:54:19 +0900

    feat: 검색 api 연동 코드

--- frontend/src/pages/SearchResultsPage/SearchResultsPage.jsx
+++ frontend/src/pages/SearchResultsPage/SearchResultsPage.jsx
@@ -3,49 +3,50 @@ import ReactPaginate from 'react-paginate'; //페이지네이션 UI
 import * as S from './SearchResults.style';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import { PlaceCard, SearchBar, Category } from '../../components';
-
-// 예시
-const places = [
-  {
-    id: 1,
-    imageUrl: 'https://blog.kakaocdn.net/dn/dizeYM/btrN5vZONwk/0ShfJor6t6KANhKzI3Qr1k/img.jpg',
-    placeName: '텅',
-    location: '서울 종로구 북촌로 701호',
-  },
-  {
-    id: 2,
-    placeName: '프로토콜 연희점',
-    location: '서울 서대문구 연희로 10길 2층',
-  },
-  { id: 3, placeName: '장소', location: '위치' },
-  { id: 4, placeName: '장소', location: '위치' },
-  { id: 5, placeName: '장소', location: '위치' },
-  { id: 6, placeName: '장소', location: '위치' },
-  { id: 7, placeName: '장소', location: '위치' },
-  { id: 8, placeName: '장소', location: '위치' },
-  { id: 9, placeName: '장소', location: '위치' },
-];
+import axios from 'axios';
+import { apiUrl } from '../../Api';
+import { useParams } from 'react-router-dom';
 
 const SearchResultsPage = () => {
+  // 페이지네이션
   const placesPerPage = 8;
   // 현재 페이지의 첫번째 항목 위치 저장
   const [currentPlaces, setCurrentPlaces] = useState([]);
   const [pageCount, setPageCount] = useState(0);
   const [placesOffset, setPlacesOffset] = useState(0);
 
-  // Simulate fetching placess from another resources.
-  // (This could be placess from props; or placess loaded in a local state
+  // API 연동
+  const { search } = useParams();
+  const [searchResults, setSearchResults] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/place/${encodeURIComponent(search)}`);
+        setSearchResults(response.data.data);
+      } catch (err) {
+        if (err.response) {
+          setError(err.response?.data?.error);
+        }
+      }
+    };
+    if (search) {
+      fetchResults();
+    }
+  }, [search]);
+
+  // Simulate fetching places from another resources.
+  // (This could be places from props; or places loaded in a local state
   // from an API endpoint with useEffect and useState)
   useEffect(() => {
     const endOffset = placesOffset + placesPerPage; //현재 페이지의 마지막 항목 위치
-    console.log(`Loading places from ${placesOffset} to ${endOffset}`);
-    setCurrentPlaces(places.slice(placesOffset, endOffset)); //현재 페이지의 데이터 추출
-    setPageCount(Math.ceil(places.length / placesPerPage)); //총 페이지 수
-  }, [placesOffset]);
+    setCurrentPlaces(searchResults.slice(placesOffset, endOffset)); //현재 페이지의 데이터 추출
+    setPageCount(Math.ceil(searchResults.length / placesPerPage)); //총 페이지 수
+  }, [placesOffset, searchResults]);
 
   const handlePageClick = event => {
     const newOffset = event.selected * placesPerPage;
-    console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
     setPlacesOffset(newOffset); //페이지 변경(시작점)
   };
 
@@ -53,9 +54,10 @@ const SearchResultsPage = () => {
     <div>
       <SearchBar />
       <Category />
+      {error && <p>{error}</p>}
       <S.PlaceCardContainer>
-        {currentPlaces.map(({ id, imageUrl, placeName, location }) => (
-          <PlaceCard key={id} imageUrl={imageUrl} placeName={placeName} location={location} />
+        {currentPlaces.map(({ postId, imageUrl, place, address }) => (
+          <PlaceCard key={postId} imageUrl={imageUrl} placeName={place} location={address} />
         ))}
       </S.PlaceCardContainer>
       <S.PaginationContainer>
